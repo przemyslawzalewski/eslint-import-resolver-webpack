6168cd1154220c9bb5da0ef2a85ac68655500f38
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2019-07-18 08:20:32 +0200

    Update dependencies

--- test/packageMains.js
+++ test/packageMains.js
@@ -14,21 +14,6 @@ describe("packageMains", function () {
       .to.equal(path.join(__dirname, 'package-mains', 'module', 'src', 'index.js'))
   })
 
-  it("captures jsnext", function () {
-    expect(webpack.resolve('./jsnext', file)).property('path')
-      .to.equal(path.join(__dirname, 'package-mains', 'jsnext', 'src', 'index.js'))
-  })
-
-  it("captures webpack", function () {
-    expect(webpack.resolve('./webpack', file)).property('path')
-      .to.equal(path.join(__dirname, 'package-mains', 'webpack', 'webpack.js'))
-  })
-
-  it("captures jam (array path)", function () {
-    expect(webpack.resolve('./jam', file)).property('path')
-      .to.equal(path.join(__dirname, 'package-mains', 'jam', 'jam.js'))
-  })
-
   it("uses configured packageMains, if provided", function () {
     expect(webpack.resolve('./webpack', file, { config: 'webpack.alt.config.js' })).property('path')
       .to.equal(path.join(__dirname, 'package-mains', 'webpack', 'index.js'))
@@ -37,11 +22,6 @@ describe("packageMains", function () {
   it("always defers to module, regardless of config", function () {
     expect(webpack.resolve('./module', file, { config: 'webpack.alt.config.js' })).property('path')
       .to.equal(path.join(__dirname, 'package-mains', 'module', 'src', 'index.js'))
-  })
-
-  it("always defers to jsnext:main, regardless of config", function () {
-    expect(webpack.resolve('./jsnext', file, { config: 'webpack.alt.config.js' })).property('path')
-      .to.equal(path.join(__dirname, 'package-mains', 'jsnext', 'src', 'index.js'))
   })
 
 })
